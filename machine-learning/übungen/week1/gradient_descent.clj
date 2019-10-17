a2cdbad875fd7454adc2a28acf69ef38acd60d4f
Laszlo Korte <me@laszlokorte.de>
2019-10-17 22:56:54 +0200

    add matrix method

--- machine-learning/übungen/week1/gradient_descent.clj
+++ machine-learning/übungen/week1/gradient_descent.clj
@@ -2,6 +2,8 @@
   (:require [com.hypirion.clj-xchart :as c]
             [clojure.core.matrix :as m]))
 
+(m/set-current-implementation :vectorz) 
+
 (defn sin-scaled [x]
   (Math/sin (* 2 Math/PI x)))
 
@@ -39,34 +41,43 @@
    theta
    data))
 
-(defn find-polynomial [points rate degree error-threshold]
+(defn find-polynomial [points rate degree]
   (let [data (map (fn [[x y]] {:x (powers x (inc degree)) :y y}) points)
         alpha rate
-        theta (vec (take (inc degree) (repeatedly rand)))
+        theta (vec (take (inc degree) (repeatedly #(rand 0.5))))
         hyp m/dot]
     (map 
     (fn [t] {:t t :error (error data t hyp)})
     (iterate #(gradient-descent data alpha %1 hyp) theta))))
     
+(defn find-polynomial-matrix [points degree]
+(let [X (map (fn [[x y]] (powers x (inc degree))) points)
+      XT (m/transpose X)
+      y (map second points)]
+      (m/mmul (m/inverse (m/mmul XT X)) XT y)))
 
 (defn draw [d]
   (let [polynome-degree d
-        step-size 0.2
-        iteration-limit 3000
-        error-threshold 0.1
-        data (gen-data 100 0.3)
+        step-size 0.03
+        iteration-limit 4000
+        data (vec (gen-data 100 0.3))
         x-range (range 0 1 0.01)
         sin (map #(vector %1 (sin-scaled %1)) x-range)
-        iterations (vec (take iteration-limit (find-polynomial data step-size polynome-degree error-threshold)))
-        errors (drop 3 (map :error iterations))
+        iterations (vec (take iteration-limit (find-polynomial data step-size polynome-degree)))
+        errors (map :error iterations)
         polynome (:t (last iterations))
-        aprox (map #(vector %1 (m/dot polynome (powers %1 (count polynome)))) x-range)]
+        polynome' (find-polynomial-matrix data polynome-degree)
+        aprox (map #(vector %1 (m/dot polynome (powers %1 (count polynome)))) x-range)
+        aprox' (map #(vector %1 (m/dot polynome' (powers %1 (count polynome')))) x-range)]
     (c/view (c/xy-chart
              {"Data Points"
               (assoc (series-from-vectors data)
                      :style {:render-style :scatter})
-              "Aproximation"
+              "Aproximation (via Iteration)"
               (assoc (series-from-vectors aprox)
+                     :style {:marker-type :none})
+              "Aproximation (via Matrix)"
+              (assoc (series-from-vectors aprox')
                      :style {:marker-type :none})
               "Sine wave"
               (assoc (series-from-vectors sin)
--- machine-learning/übungen/week1/project.clj
+++ machine-learning/übungen/week1/project.clj
@@ -1,6 +1,7 @@
 (defproject gradient-descent "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.hypirion/clj-xchart "0.2.0"]
+                 [net.mikera/vectorz-clj "0.48.0"]
                  [net.mikera/core.matrix "0.62.0"]]
   :source-paths ["."]
   :repl-options {:init-ns gradient-descent}
